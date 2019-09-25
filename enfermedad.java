05167f43766a4caeb1502c96d258ee641d3828d6
majomr <mor19145@uvg.edu.gt>
2019-09-25 16:19:37 -0600

    clase enfermedad y el while para pedir sintomas en main

---
+++ enfermedad.java
@@ -0,0 +1,49 @@
+/*Proyecto de POO
+Grupo:
+Maria Jose Morales
+Andres Paiz
+Jose Ramos
+Luis Pedro García
+Maria Montoya
+Enfermedades*/
+
+class enfermedad{
+	
+	public enfermedad(){
+		
+	}
+	//dependiendo el numero de sintoma es la enfermedad que puede tener
+	public String diagnostico(int sintoma){
+		if(sintoma == 1	)
+			return "Neumonia";
+		else if()
+			return "";
+		else if()
+			return "";
+		else if()
+			return "";
+		else if()
+			return "";
+		else if()
+			return "";
+		else if()
+			return "";
+		else if()
+			return "";
+		
+				//Estos son os sintomas del menu en los links estas las enfermedades que hay que usar y se pueden agregar. En los ifs puede haber as de 1 sintoma con AND 
+				/*System.out.println("1. Tos");
+				System.out.println("2. Dolor de garganta");
+				System.out.println("3. Dolor de oido");
+				System.out.println("4. Dolores de cabeza");
+				System.out.println("5. Dolores al orinar, picazón en genitales");
+				System.out.println("7. Vomitos y diarrea");
+				System.out.println("8. Cambio de peso repentino");
+				System.out.println("9. Presion alta");
+				System.out.println("10. Escalofrios, dolor intenso en torax");
+				System.out.println("11. Fiebre alta, ronchas o rash en la piel");*/
+				//12. congestion 
+				//https://www.prensalibre.com/vida/salud-y-familia/enfermedadaes-que-afectan-a-los-guatemaltecos/
+				//https://www.qualipharm.info/estas-son-las-5-enfermedades-mas-comunes-en-guatemala/
+	} 	
+}
