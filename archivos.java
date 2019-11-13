bb6cd6e5216b44c324b848a473c5ba75d276c8d0
marisaamv <mon19169@uvg.edu.gt>
2019-11-13 12:12:35 -0600

    Hoteles datos

--- archivos.java
+++ archivos.java
@@ -270,4 +270,263 @@ public class archivos{
 			System.out.println("Mensaje de la excepción: " + ex.getMessage());
 		}
 	}
+	public void DBh(int dept){
+		if (dept==1){
+			this.archivo= "hoteles1.txt";
+		}
+		else{
+			if(dept==2){
+				this.archivo="hoteles2.txt";
+			}
+			else{
+				if (dept==3){
+					this.archivo= "hoteles3.txt";
+				}
+				else{
+					if(dept==4){
+						this.archivo="hoteles4.txt";
+					}
+					else{				
+						if (dept==5){
+							this.archivo= "hoteles5.txt";
+						}
+						else{
+							if(dept==6){
+								this.archivo="hoteles6.txt";
+							}
+							else{
+								if (dept==7){
+									this.archivo= "hoteles7.txt";
+								}
+								else{
+									if(dept==8){
+										this.archivo="hoteles8.txt";
+									}
+									else{	
+										if (dept==9){
+											this.archivo= "hoteles9.txt";
+										}
+										else{
+											if(dept==10){
+												this.archivo="hoteles10.txt";
+											}
+											else{
+												if (dept==11){
+													this.archivo= "hoteles11.txt";
+												}
+												else{
+													if(dept==12){
+														this.archivo="hoteles12.txt";
+													}
+													else{				
+														if (dept==13){
+															this.archivo= "hoteles13.txt";
+														}
+														else{
+															if(dept==14){
+																this.archivo="hoteles14.txt";
+															}
+															else{
+																if (dept==15){
+																	this.archivo= "hoteles15.txt";
+																}
+																else{
+																	if(dept==16){
+																		this.archivo="hoteles16.txt";
+																	}
+																	else{	
+																		if (dept==17){
+																			this.archivo= "hoteles17.txt";
+																		}
+																		else{
+																			if(dept==18){
+																				this.archivo="hoteles18.txt";
+																			}
+																			else{
+																				if (dept==19){
+																					this.archivo= "hoteles19.txt";
+																				}
+																				else{
+																					if(dept==20){
+																						this.archivo="hoteles20.txt";
+																					}
+																					else{				
+																						if (dept==21){
+																							this.archivo= "hoteles21.txt";
+																						}
+																						else{
+																							this.archivo="hoteles22.txt";
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		File fichero = new File(archivo);
+		Scanner s = null;
+		String linea = "";
+		try{
+			s = new Scanner(fichero);
+			while(s.hasNextLine()){
+				linea+= s.nextLine();
+				System.out.println("\n" + linea);
+			}
+		}
+		catch(Exception ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
+		}
+		finally{
+		// Cerramos el fichero tanto si la lectura ha sido correcta o no
+			try {
+				if (s != null)
+					s.close();
+			
+			} 
+			catch (Exception ex2) {
+				System.out.println("Mensaje 2: " + ex2.getMessage());
+			}
+		}
+	}
+	public class addDBh(String info, int dept){
+				if (dept==1){
+			this.archivo= "hoteles1.txt";
+		}
+		else{
+			if(dept==2){
+				this.archivo="hoteles2.txt";
+			}
+			else{
+				if (dept==3){
+					this.archivo= "hoteles3.txt";
+				}
+				else{
+					if(dept==4){
+						this.archivo="hoteles4.txt";
+					}
+					else{				
+						if (dept==5){
+							this.archivo= "hoteles5.txt";
+						}
+						else{
+							if(dept==6){
+								this.archivo="hoteles6.txt";
+							}
+							else{
+								if (dept==7){
+									this.archivo= "hoteles7.txt";
+								}
+								else{
+									if(dept==8){
+										this.archivo="hoteles8.txt";
+									}
+									else{	
+										if (dept==9){
+											this.archivo= "hoteles9.txt";
+										}
+										else{
+											if(dept==10){
+												this.archivo="hoteles10.txt";
+											}
+											else{
+												if (dept==11){
+													this.archivo= "hoteles11.txt";
+												}
+												else{
+													if(dept==12){
+														this.archivo="hoteles12.txt";
+													}
+													else{				
+														if (dept==13){
+															this.archivo= "hoteles13.txt";
+														}
+														else{
+															if(dept==14){
+																this.archivo="hoteles14.txt";
+															}
+															else{
+																if (dept==15){
+																	this.archivo= "hoteles15.txt";
+																}
+																else{
+																	if(dept==16){
+																		this.archivo="hoteles16.txt";
+																	}
+																	else{	
+																		if (dept==17){
+																			this.archivo= "hoteles17.txt";
+																		}
+																		else{
+																			if(dept==18){
+																				this.archivo="hoteles18.txt";
+																			}
+																			else{
+																				if (dept==19){
+																					this.archivo= "hoteles19.txt";
+																				}
+																				else{
+																					if(dept==20){
+																						this.archivo="hoteles20.txt";
+																					}
+																					else{				
+																						if (dept==21){
+																							this.archivo= "hoteles21.txt";
+																						}
+																						else{
+																							this.archivo="hoteles22.txt";
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		FileWriter ficheros = null;
+		File archivos = new File(archivo);
+		Scanner s = null;
+		String linea = "";
+		try{
+			s = new Scanner(archivo);
+			while(s.hasNextLine()){
+				linea+= s.nextLine();
+			}
+			ficheros= new FileWriter(archivo);
+			linea= linea + "\n" + info;
+			ficheros.write(linea)
+			
+			ficheros.close();
+		}
+		catch (Exception ex) {
+			System.out.println("Mensaje de la excepción: " + ex.getMessage());
+		}
+	}
+	
 }						
